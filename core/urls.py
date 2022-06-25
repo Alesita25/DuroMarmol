e3bfc7bb64330ac30fa0fbddd0194580d03cc54d
Alesita25 <alej.aranguiz@duocuc.cl>
2022-06-25 02:15:13 -0400

    3

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,16 @@
 from django.urls import URLPattern, path
-from .views import home, clientes, mod_clientes,bor_clientes
+from .views import guia_despacho, home, clientes, mod_clientes,bor_clientes,cotizaciones,ingreso_pedidos,orden_compra,proveedores,inventario
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('clientes', clientes, name="clientes"),
     path('mod_clientes/<id>',mod_clientes,name='mod_clientes'),
-    path('bor_clientes/<id>',bor_clientes,name='bor_clientes')
+    path('bor_clientes/<id>',bor_clientes,name='bor_clientes'),
+    path('cotizaciones', cotizaciones, name="cotizaciones"),
+    path('ingreso_pedidos', ingreso_pedidos, name="ingreso_pedidos"),
+    path('orden_compra', orden_compra, name="orden_compra"),
+    path('proveedores', proveedores, name="proveedores"),
+    path('guia_despacho', guia_despacho, name="guia_despacho"),
+    path('inventario', inventario, name="inventario")
 ]
--- core/views.py
+++ core/views.py
@@ -7,6 +7,21 @@ from .models import Clientes
 def home(request):
     return render(request, 'core/home.html')
 
+def cotizaciones(request):
+    return render(request, 'core/cotizaciones.html')
+
+def ingreso_pedidos(request):
+    return render(request, 'core/ingreso_pedidos.html')
+
+def orden_compra(request):
+    return render(request, 'core/orden_compra.html')
+def proveedores(request):
+    return render(request, 'core/proveedores.html')
+def guia_despacho(request):
+    return render(request, 'core/guia_despacho.html')
+def inventario(request):
+    return render(request, 'core/inventario.html')
+
 # def clientes(request):
 #     return render(request, 'core/clientes.html')
 
